c22530028226ec7fbb804db0bdfe0049be578519
puvanakopis <puvanakopis@gmail.com>
2026-05-28 16:48:45 +0530

    feat: complete home page integration

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+"use client";
+
+import Hero from "./components/Hero";
+import FeaturedPhones from "./components/FeaturedPhones";
+import Offers from "./components/Offers";
+import WhyChooseUs from "./components/WhyChooseUs";
+import Reviews from "./components/Reviews";
+import LifestyleBanner from "./components/LifestyleBanner";
 
 export default function Home() {
   return (
-    <div>
-      iStore
-    </div>
+    <main>
+      <Hero />
+      <FeaturedPhones />
+      <Offers />
+      <WhyChooseUs />
+      <Reviews />
+      <LifestyleBanner />
+    </main>
   );
 }
